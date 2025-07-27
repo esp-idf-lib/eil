22d8956925afef021da714723adc96df9e7c06fb
Tomoyuki Sakurai <y@trombik.org>
2025-07-27 11:34:24 +0700

    feat: generate README.md from template

---
+++ lib/eil/component.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module EIL
+  # A class represents esp-idf-lib components
+  class Component
+    attr_reader :name
+
+    ORG = "esp-idf-lib"
+    GITHUB_URL = "https://github.com"
+    GITHUB_PAGES_URL = "https://esp-idf-lib.github.io"
+    ESP_COMPONENT_REGISTRY_URL = "https://components.espressif.com"
+
+    def initialize(name)
+      @name = name.chomp
+    end
+
+    def org
+      ORG
+    end
+
+    def fqdn
+      "#{org}/#{name}"
+    end
+
+    def repo_url
+      "#{GITHUB_URL}/#{fqdn}"
+    end
+
+    def esp_url
+      "#{ESP_COMPONENT_REGISTRY_URL}/components/#{fqdn}"
+    end
+
+    def doc_url
+      "#{GITHUB_PAGES_URL}/#{name}"
+    end
+
+    def issues_url
+      "#{repo_url}/issues"
+    end
+
+    def badge_svg_url(workflow_file)
+      "#{workflow_url(workflow_file)}/badge.svg"
+    end
+
+    def workflow_url(workflow_file)
+      "#{repo_url}/actions/workflows/#{workflow_file}"
+    end
+
+    def badge(workflow_file)
+      workflow_name = workflow_file.split(".").first
+      "[![#{workflow_name}](#{badge_svg_url(workflow_file)})](#{workflow_url(workflow_file)})"
+    end
+  end
+end
